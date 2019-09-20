1ea1dc3237d1abd0034b859fce8cf6e6789edcd9
JohnFury77 <furijani99@gmail.com>
2019-09-20 12:34:36 +0200

    1. feladat

--- Github JohnFury/Program.cs
+++ Github JohnFury/Program.cs
@@ -10,7 +10,22 @@ namespace Github_JohnFury
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My first Git project");
+            Console.WriteLine("Kérek 10 számot.");
+            int[] szamok=new int[10];
+            int seged = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Írja be a "+(i+1)+". számot");
+                szamok[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (seged<szamok[i])
+                {
+                    seged = szamok[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: "+seged);
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
             
